a66d997de20ed22483ec2f3f42b4241a9637b793
asif-kamal <asif.kamal1489@gmail.com>
2026-07-03 12:48:53 -0500

    created a standard deck of cards for Collections methods manipulation

--- src/main/java/com/asifkamal/Main.java
+++ src/main/java/com/asifkamal/Main.java
@@ -1,10 +1,13 @@
 package com.asifkamal;
 
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-
+        List<Card> deck = Card.getStandardDeck();
+        Card.printDeck(deck);
     }
 }
